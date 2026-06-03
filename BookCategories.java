f6a304476f8b4e65f86e8486d1494f22f71a93be
nuratiqahmnoor-pixel <nuratiqahmnoor@gmail.com>
2026-06-03 23:28:29 +0800

    Create BookCategories.java

---
+++ BookCategories.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package noorbookstore;
+
+/**
+ *
+ * @author User
+ */
+
+public class BookCategories {
+
+    // Attributes
+    private String categoryID;
+    private String categoryName;
+    private String categoryDescription;
+    
+    // Constructor
+    public BookCategories(String categoryID, String categoryName, String categoryDescription) {
+
+        this.categoryID = categoryID;
+        this.categoryName = categoryName;
+        this.categoryDescription = categoryDescription;
+    }
+
+    public String getCategoryID() {return categoryID;}
+    public void setCategoryID(String categoryID) {this.categoryID = categoryID;}
+      
+    public String getCategoryName() {return categoryName;}
+    public void setCategoryName(String categoryName) {this.categoryName = categoryName;}
+
+    public String getCategoryDescription() {return categoryDescription;}
+    public void setCategoryDescription(String categoryDescription) {this.categoryDescription = categoryDescription;}
+
+    public boolean isIslamicCategory() {
+
+        return categoryName.equalsIgnoreCase("Quran") 
+                || categoryName.equalsIgnoreCase("Hadith") 
+                || categoryName.equalsIgnoreCase("Tafsir")
+                || categoryName.equalsIgnoreCase("Fiqh") 
+                || categoryName.equalsIgnoreCase("Usuluddin");
+    }
+
+    @Override
+    public String toString() {
+
+        return categoryName;
+    }
+}
