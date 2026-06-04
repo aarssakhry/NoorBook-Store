a07c79ef46e23534909c451b71368dbde74745ee
zulaikhananii <zulaikhananii@gmail.com>
2026-06-05 07:23:55 +0800

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package noorbookstore;
+
+/**
+ *
+ * @author USER
+ */
+//INHERITANCE
+class Admin extends User {
+    
+    //ENCAPSULATION
+    private String role; //e.g. "Super Admin", "Product Manager"
+    
+    //Constructor
+    public Admin(String userID, String name, String email, String password, String role){
+      super(userID, name, email, password);
+      this.role = role;
+    }
+    
+    //Getter method
+    public String getRole(){
+        return role;
+    }
+    
+    //Setter method
+    public void setRole(String role){
+        this.role = role;
+    }
+    
+    //POLYMORPHISM
+    //displays an admin-specific dashboard
+    @Override
+    public void showDashboard(){
+        System.out.println("===================================================");
+        System.out.println("           NoorBook Store - Admin Panel           ");
+        System.out.println("===================================================");
+        System.out.println(" Admin Name: " + getName());
+        System.out.println(" Role: " + role);
+        System.out.println(" Access: Add / Edit/ Delete Products");
+        System.out.println("       : View All Customer Orders");
+        System.out.println("===================================================");
+    }
+}
